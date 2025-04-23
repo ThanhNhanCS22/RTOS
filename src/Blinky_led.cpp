279b39cc67c035a48cd35bf09867171a8f4ab1a6
Thanh Nhan <thanhnhan1662004@gmail.com>
2025-04-23 15:04:50 +0700

    v6

--- src/Blinky_led.cpp
+++ src/Blinky_led.cpp
@@ -1,19 +1,22 @@
 #include "Blinky_led.h"
 #define ledPin GPIO_NUM_48  
+#define init 0 
+#define off 1  
+#define on 2 
 
 int ledStatus = 0 ; 
 
 void blinkyLedRun() {
     switch(ledStatus) {
-        case 0 : 
+        case init  : 
             pinMode(ledPin  , OUTPUT ) ;
             ledStatus = 1 ;   
            
-        case 1 :   
+        case off :   
             digitalWrite(ledPin, LOW ) ;
             ledStatus = 2  ; 
             break ; 
-        case 2 : 
+        case on : 
             digitalWrite(ledPin, HIGH ) ;
             ledStatus = 1   ; 
             break ; 
